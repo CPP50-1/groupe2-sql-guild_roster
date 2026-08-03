db3fb1a880a12054621c015e574aa6a00bf9488c
Mithirsan Mathyvarnan <34122710+Mithirsan@users.noreply.github.com>
2026-08-03 09:18:42 +0200

    fix: use of reserved keyword need commas

--- static/sql/data_model.sql
+++ static/sql/data_model.sql
@@ -7,16 +7,16 @@ CREATE TABLE roster (
     name VARCHAR(100) UNIQUE NOT NULL
 );
 
-CREATE TABLE character (
+CREATE TABLE "character" (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) UNIQUE NOT NULL,
     hp INTEGER NOT NULL CHECK (hp >= 0),
     level INTEGER NOT NULL CHECK (level BETWEEN 1 AND 100),
-    type CHARACTER_TYPE NOT NULL,
-    roster_id INTEGER REFERENCES roster(id) ON DELETE SET NULL
-    -- SET NULL because character can exist outside of roaster
+    character_type CHARACTER_TYPE NOT NULL,
+    roster_id INTEGER NULL REFERENCES roster(id) ON DELETE SET NULL
+    -- SET NULL because character can exist outside of roster
 );
-CREATE INDEX idx_character_roster_id ON character(roster_id);
+CREATE INDEX idx_character_roster_id ON "character"(roster_id);
 
 CREATE TABLE ability (
     id SERIAL PRIMARY KEY,
@@ -33,7 +33,7 @@ CREATE TABLE ability (
 );
 
 CREATE TABLE character_ability (
-    character_id INTEGER REFERENCES character(id) ON DELETE CASCADE,
+    character_id INTEGER REFERENCES "character"(id) ON DELETE CASCADE,
     -- if character is deleted. no ability should be linked to it
     ability_id INTEGER REFERENCES ability(id) ON DELETE CASCADE,
     -- if ability is deleted. no character has it
@@ -49,7 +49,7 @@ CREATE TABLE item (
 );
 
 CREATE TABLE character_item (
-    character_id INTEGER REFERENCES character(id) ON DELETE CASCADE,
+    character_id INTEGER REFERENCES "character"(id) ON DELETE CASCADE,
     -- if character is deleted, it loses his items
     item_id INTEGER REFERENCES item(id) ON DELETE CASCADE,
     -- if item is deleted, no character should own it
@@ -66,7 +66,7 @@ CREATE TABLE quest (
 );
 
 CREATE TABLE character_quest (
-    character_id INTEGER REFERENCES character(id) ON DELETE CASCADE,
+    character_id INTEGER REFERENCES "character"(id) ON DELETE CASCADE,
     quest_id INTEGER REFERENCES quest(id) ON DELETE CASCADE,
     PRIMARY KEY (character_id, quest_id)
 );
